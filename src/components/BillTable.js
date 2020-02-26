f422428fb3b9285383bd7ebc91f80a05be11412f
Jay <jay@MWCSB2s-MacBook-Pro.local>
2020-02-26 15:13:16 -0800

    Added-searchButton-vetoButton-materialU

--- src/components/BillTable.js
+++ src/components/BillTable.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import './../App.css';
+// import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
-const BillTable = ({  bills }) => {
+const BillTable = ({ bills }) => {
     console.log('Sent to component, bills:', bills);
     return (
         <React.Fragment>
+            <TableContainer component={Paper}>
+                <Table  aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="center">Measure Number</TableCell>
+                            <TableCell align="center">Signed or Vetoed</TableCell>
+                            <TableCell align="center">Voter support</TableCell>
+                            <TableCell align="center">Date</TableCell>
+                            <TableCell align="center">Relating to clause</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+
+
+                    </TableBody>
+                </Table>
+            </TableContainer>
+
             <table className="info">
                 <thead>
                     <tr>
